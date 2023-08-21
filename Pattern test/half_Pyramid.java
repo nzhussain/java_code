ea4239397cc031bfd6fb3db231341e919a20ee6e
nzhussain <nzhussain021@gmail.com>
2023-08-21 21:19:50 +0530

    commit 6th

---
+++ Pattern test/half_Pyramid.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class half_Pyramid {
+    public static void main(String args[]){
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter the input(n):");
+        int n = obj.nextInt();
+        /* <<<---half_Pyramid--->>> */
+        //Outer Loop(row)
+        for(int r=1;r<=n;r++){
+            //Inner Loop(coloum)
+            for(int c=1;c<=r;c++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
